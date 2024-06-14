61758a0a64d569da912eae0620ebdfd9bc9d6648
hoicryu <hoicryu@gmail.com>
2024-06-14 23:53:37 +0900

    [Updated] Calendar UI

--- src/common/hooks/useCalendar.ts
+++ src/common/hooks/useCalendar.ts
@@ -8,19 +8,19 @@ const useCalendar = (ref, containerId: string) => {
   const setReservation = useSetRecoilState(reservationState);
   const onPageInit = () => {
     const $ = f7.$;
-
-    const monthNames = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
+    const monthNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
     ref.current = f7.calendar.create({
       containerEl: `#${containerId}`,
       value: [new Date()],
+      firstDay: 0,
       renderToolbar() {
         return `
-          <div class="toolbar calendar-custom-toolbar">
-            <div class="toolbar-inner">
+          <div class="toolbar calendar-custom-toolbar mb-3">
+            <div class="toolbar-inner bg-white">
               <div class="left">
                 <a  class="link icon-only"><i class="icon icon-back"></i></a>
               </div>
-              <div class="center"></div>
+              <div class="center text-lg font-semibold tracking-wide"></div>
               <div class="right">
                 <a  class="link icon-only"><i class="icon icon-forward"></i></a>
               </div>
@@ -28,18 +28,29 @@ const useCalendar = (ref, containerId: string) => {
           </div>
         `.trim();
       },
+
       on: {
         init(c) {
-          $('.calendar-custom-toolbar .center').text(`${monthNames[c.currentMonth]}, ${c.currentYear}`);
+          $('.calendar-custom-toolbar .center').text(`${c.currentYear}.${monthNames[c.currentMonth]}`);
           $('.calendar-custom-toolbar .left .link').on('click', () => {
             ref.current.prevMonth();
           });
           $('.calendar-custom-toolbar .right .link').on('click', () => {
             ref.current.nextMonth();
           });
+          $('.calendar-day-number').forEach((el) => {
+            const day = $(el).text().replace('일', '');
+            $(el).text(day);
+          });
         },
         monthYearChangeStart(c) {
-          $('.calendar-custom-toolbar .center').text(`${monthNames[c.currentMonth]}, ${c.currentYear}`);
+          $('.calendar-custom-toolbar .center').text(`${c.currentYear}.${monthNames[c.currentMonth]}`);
+          $('.calendar-day-number').forEach((el) => {
+            if ($(el).text().includes('일')) {
+              const day = $(el).text().replace('일', '');
+              $(el).text(day);
+            }
+          });
         },
         dayClick(calendar, dayEl, year, month, day) {
           const dateobj = { date: `${year}-${month + 1}-${day}` };
